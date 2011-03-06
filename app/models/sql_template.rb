4de20bdef88c01e454c832bbfe8d3e998e66797e
Charles Feduke <charles.feduke@gmail.com>
2011-03-06 13:30:36 -0500

    passing unit tests

--- app/models/sql_template.rb
+++ app/models/sql_template.rb
@@ -1,9 +1,8 @@
 class SqlTemplate < ActiveRecord::Base
   validates :body, :path, :presence => true
-  
   validates :format,    :inclusion => Mime::SET.symbols.map(&:to_s)
   validates :locale,    :inclusion => I18n.available_locales.map(&:to_s)
-  validates :handler,   :inclusion => ActionView:Template::Handlers.extensions.map(&:to_s)
+  validates :handler,   :inclusion => ActionView::Template::Handlers.extensions.map(&:to_s)
   
   class Resolver < ActionView::Resolver
     protected
